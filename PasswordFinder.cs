f7a7a132c1a18541f988f2dfe344106084fa35c5
Nizar-ST <nizarzayto@gmail.com>
2024-06-13 13:01:04 +0100

    Add project files.

---
+++ PasswordFinder.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace PasswordManager {
+    internal class PasswordManager {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
